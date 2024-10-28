26e490255e8236e2ad21a93d2afa3a2cfa857c9f
Roxanne Zhao <roxannezhao199@gmail.com>
2024-10-28 21:01:12 +0200

    change portfolio link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
-import profileImg from '../assets/profile.jpeg'
+import React from "react";
+import profileImg from "../assets/profile.jpeg";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-900 via-gray-900 to-black text-[#E1DFDF]'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-3xl font-bold inline border-b-4 border-gray-500'>About</p>
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-900 via-gray-900 to-black text-[#E1DFDF]"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-3xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
         </div>
-        <div className='about__img mt-1 flex justify-center items-center'>
+        <div className="about__img mt-1 flex justify-center items-center">
           <img src={profileImg} alt="" />
         </div>
-        <p className='text-lg mt-2 font-bold flex justify-center items-center'>
+        <p className="text-lg mt-2 font-bold flex justify-center items-center">
           Front-end Developer | Full-stack Developer | Freelancer
         </p>
 
         <br />
 
-        <p className='text-base text-gray-500'>
-          I worked as a project engineer👷🏻‍♀️ in the manufacturing and construction industry for many years. It was by chance that my programming learning journey started in early 2021. I instantly fell in love with it both the idea of using logic on one hand and also being very creative on the other hand. I enjoy problem-solving and it gives me a sense of achievement.<br />
-          Changing careers midway is not an easy task. But I have the confidence and the faith in myself to get to where I want to be despite the industry or the people that I will be working with. Now I am a junior web developer👩🏻‍💻. I am passionate about building beautiful and functional web pages and mini-programs. For sure there are still a lot of unknowns, but that’s what excites me and motivates me to be a better developer in the future.✌🏻
+        <p className="text-base text-gray-500">
+          I worked as a project engineer👷🏻‍♀️ in the manufacturing and construction
+          industry for many years. It was by chance that my programming learning
+          journey started in 2021. I instantly fell in love with it both the
+          idea of using logic on one hand and also being very creative on the
+          other hand. I enjoy problem-solving and it gives me a sense of
+          achievement.
+          <br />
+          Changing careers midway is not an easy task. But I have the confidence
+          and the faith in myself to get to where I want to be despite the
+          industry or the people that I will be working with. I am passionate
+          about building beautiful and functional web pages and applications.
+          For sure there are still a lot of unknowns, but that’s what excites me
+          and motivates me to be a better developer in the future.✌🏻
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,39 +1,50 @@
-import React from 'react'
-import synapse from '../assets/portfolio/synapse.png'
-import furryfun from '../assets/portfolio/furryfun.png'
-import mengcupcake from '../assets/portfolio/mengcupcake.png'
-import musicPlayer from '../assets/portfolio/musicPlayer.png'
-import { SiRubyonrails } from "react-icons/si"
-import { SiReact } from "react-icons/si"
-import { DiJavascript1 } from "react-icons/di"
-
+import React from "react";
+import synapse from "../assets/portfolio/synapse.png";
+import furryfun from "../assets/portfolio/furryfun.png";
+import mengcupcake from "../assets/portfolio/mengcupcake.png";
+import musicPlayer from "../assets/portfolio/musicPlayer.png";
+import { SiRubyonrails } from "react-icons/si";
+import { SiReact } from "react-icons/si";
+import { DiJavascript1 } from "react-icons/di";
 
 const Portfolio = () => {
-
   const portfolio = [
     {
       id: 1,
       src: synapse,
       title: "Synapse - Wechat MP",
-      icon: <> <SiRubyonrails size={30} /> <DiJavascript1 size={20} /> </>,
-      demoLink: "https://synapse.roxannezhao.com",
-      codeLink: "https://github.com/RoxanneZhao199/Synapse_frontend"
+      icon: (
+        <>
+          {" "}
+          <SiRubyonrails size={30} /> <DiJavascript1 size={20} />{" "}
+        </>
+      ),
+      // demoLink: "https://synapse.roxannezhao.com",
+      demoLink: "https://portfolio-work1.pages.dev",
+      codeLink: "https://github.com/RoxanneZhao199/Synapse_frontend",
     },
     {
       id: 2,
       src: furryfun,
       title: "FurryFun - Wechat MP",
-      icon: <> <SiRubyonrails size={30} /> <DiJavascript1 size={20} /> </>,
-      demoLink: "https://furryfun.roxannezhao.com",
-      codeLink: "https://github.com/SueLuoHang/Furryfunbackend"
+      icon: (
+        <>
+          {" "}
+          <SiRubyonrails size={30} /> <DiJavascript1 size={20} />{" "}
+        </>
+      ),
+      // demoLink: "https://furryfun.roxannezhao.com",
+      demoLink: "https://portfolio-work2.pages.dev",
+      codeLink: "https://github.com/SueLuoHang/Furryfunbackend",
     },
     {
       id: 3,
       src: mengcupcake,
       title: "Meng's Cupcake",
-      icon: <SiReact size={25}/>,
-      demoLink: "https://mengcupcake.roxannezhao.com",
-      codeLink: "https://github.com/RoxanneZhao199/cake-store"
+      icon: <SiReact size={25} />,
+      // demoLink: "https://mengcupcake.roxannezhao.com",
+      demoLink: "https://mengcupcake.pages.dev",
+      codeLink: "https://github.com/RoxanneZhao199/cake-store",
     },
     {
       id: 4,
@@ -41,47 +52,57 @@ const Portfolio = () => {
       title: "Customize Music Player",
       icon: <DiJavascript1 size={25} />,
       demoLink: "https://custom-music-player.pages.dev/",
-      codeLink: "https://github.com/RoxanneZhao199/Custom-Music-Player"
-    }
-  ]
+      codeLink: "https://github.com/RoxanneZhao199/Custom-Music-Player",
+    },
+  ];
 
   return (
-    <div name='portfolio' className='bg-gradient-to-b from-black via-black to-gray-800 w-full text-[#E1DFDF] md:h-screen'>
-      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
-        <div className='pb-8'>
-          <p className='text-3xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-          <p className='py-2'>Check out some of my recent work</p>
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-black via-black to-gray-800 w-full text-[#E1DFDF] md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-3xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-2">Check out some of my recent work</p>
         </div>
 
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-8'>
-        {
-          portfolio.map(({ id, src, title, icon, demoLink, codeLink }) => (
-            <div key={id} style={{ backgroundImage: `url(${src})` }} className='shadow-md shadow-gray-600 rounded-lg group container flex justify-center items-center mx-auto content-div'>
-
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8">
+          {portfolio.map(({ id, src, title, icon, demoLink, codeLink }) => (
+            <div
+              key={id}
+              style={{ backgroundImage: `url(${src})` }}
+              className="shadow-md shadow-gray-600 rounded-lg group container flex justify-center items-center mx-auto content-div"
+            >
               {/* Hover Effects */}
-              <div className='opacity-0 group-hover:opacity-100 px-10'>
-                <span className='text-2xl font-bold text-white tracking-wider'>
+              <div className="opacity-0 group-hover:opacity-100 px-10">
+                <span className="text-2xl font-bold text-white tracking-wider">
                   {title}
-                  <p className='flex items-center justify-evenly text-yellow-700'>{icon}</p>
+                  <p className="flex items-center justify-evenly text-yellow-700">
+                    {icon}
+                  </p>
                 </span>
-                <div className='pt-8 text-center'>
-                  <a href={demoLink} target='_blank' rel="noreferrer">
-                    <button className='text-center rounded-md px-4 py-2 m-2 bg-[#E1DFDF] text-gray-700 font-bold text-lg duration-200 hover:scale-105'>Demo</button>
+                <div className="pt-8 text-center">
+                  <a href={demoLink} target="_blank" rel="noreferrer">
+                    <button className="text-center rounded-md px-4 py-2 m-2 bg-[#E1DFDF] text-gray-700 font-bold text-lg duration-200 hover:scale-105">
+                      Demo
+                    </button>
                   </a>
-                  <a href={codeLink} target='_blank' rel="noreferrer">
-                    <button className='text-center rounded-md px-4 py-2 m-2 bg-[#E1DFDF] text-gray-700 font-bold text-lg duration-200 hover:scale-105'>Code</button>
+                  <a href={codeLink} target="_blank" rel="noreferrer">
+                    <button className="text-center rounded-md px-4 py-2 m-2 bg-[#E1DFDF] text-gray-700 font-bold text-lg duration-200 hover:scale-105">
+                      Code
+                    </button>
                   </a>
-
                 </div>
               </div>
             </div>
-        ))
-          }
-
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
